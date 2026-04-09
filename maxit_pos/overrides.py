ac58578881bf94cabc7c506551265453e7a7205b
Madcheese101 <hax.grenade@gmail.com>
2026-04-09 16:53:24 +0200

    fix: override check_open_pos_exists in POSOpeningEntry to check for the current user instead of global.

--- maxit_pos/overrides.py
+++ maxit_pos/overrides.py
@@ -3,6 +3,7 @@ from frappe.utils import cint, flt, get_link_to_form, nowtime
 
 import erpnext.accounts.doctype.pos_invoice.pos_invoice as pos_invoice_file
 from erpnext.controllers.selling_controller import SellingController
+from erpnext.accounts.doctype.pos_opening_entry.pos_opening_entry import POSOpeningEntry
 
 def custom_get_bin_qty(item_code, warehouse):
     actual_qty = 0
@@ -20,6 +21,20 @@ def custom_get_bin_qty(item_code, warehouse):
 
     return actual_qty
 
+def check_open_pos_exists(self):
+    if frappe.db.exists("POS Opening Entry", {
+        "pos_profile": self.pos_profile, 
+        "user": self.user,
+        "status": "Open",
+        "name": ["!=", self.name] if self.name else None
+        }):
+        frappe.throw(
+            title=_("POS Opening Entry Exists"),
+            msg=_(
+                "{0} is open. Close the POS or cancel the existing POS Opening Entry to create a new POS Opening Entry."
+            ).format(frappe.bold(self.pos_profile)),
+        )
+
 def validate_max_discount_(self):
     for d in self.get("items"):
         if d.item_code:
@@ -30,3 +45,4 @@ def validate_max_discount_(self):
 def override_methods():
     pos_invoice_file.get_bin_qty = custom_get_bin_qty
     SellingController.validate_max_discount = validate_max_discount_
+    POSOpeningEntry.check_open_pos_exists = check_open_pos_exists
