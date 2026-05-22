cc01a5b72d5f9685b3d1933d3995cb7668a826c7
Madcheese101 <hax.grenade@gmail.com>
2026-05-22 20:19:25 +0200

    fix: Enhance Stock Entry with user warehouse validation and alerts for missing defaults

--- maxit_pos/boot.py
+++ maxit_pos/boot.py
@@ -4,11 +4,15 @@ def extend_boot_info(bootinfo):
         set_user_branch_in_session()
 
     branch = frappe.session.data.get("user_branch", None)
+    user_warehouse = frappe.session.data.get("user_warehouse", None)
     bootinfo["user_branch"] = branch
+    bootinfo["user_warehouse"] = user_warehouse
 
 def set_user_branch_in_session():
     branch = frappe.db.get_value("Employee", {"user_id": frappe.session.user}, "branch")
     # Adding data to the session
     frappe.session.data["user_branch"] = branch
+    if branch:
+        frappe.session.data["user_warehouse"] = frappe.db.get_value("Branch", branch, "material_warehouse")
     # Persisting the change to the database and cache  
     frappe.local.session_obj.update(force=True)
--- maxit_pos/public/js/doctypes/stock_entry.js
+++ maxit_pos/public/js/doctypes/stock_entry.js
@@ -1,10 +1,16 @@
 let OUTGOING = ''
 const IS_SUPER_USER = frappe.user_roles.includes("System Manager") || frappe.user_roles.includes("Accounts Manager")
+const USER_BRANCH = frappe.boot.user_branch;
 const USER_WAREHOUSE = frappe.boot.user_warehouse;
 
 frappe.ui.form.on('Stock Entry', {
 	refresh(frm) {
         OUTGOING = frm.doc.outgoing_stock_entry
+        if(!USER_WAREHOUSE){
+            frappe.alert(
+                "Your user does not have a default warehouse set. Please contact your administrator.", 
+                {indicator: 'orange'});
+        }
 		if(!OUTGOING && frm.is_new()){
             frm.set_value("stock_entry_type", "Material Transfer");
         }
@@ -16,7 +22,7 @@ frappe.ui.form.on('Stock Entry', {
                 frm.set_value("to_warehouse", result.message);
             });
         }
-        if(IS_SUPER_USER && frm.is_dirty()){
+        if(IS_SUPER_USER && frm.is_dirty() && !USER_WAREHOUSE){
             frm.set_df_property("from_branch", "read_only", false);
         }
 	},
@@ -26,7 +32,8 @@ frappe.ui.form.on('Stock Entry', {
         }
     },
     add_to_transit(frm){
-        if(frm.is_new() && !OUTGOING && !IS_SUPER_USER){
+        if(frm.is_new() && !OUTGOING && !IS_SUPER_USER && USER_WAREHOUSE){
+            frm.set_value("from_branch", USER_BRANCH);
             frm.set_value("from_warehouse", USER_WAREHOUSE);
         }
     },
@@ -42,5 +49,18 @@ frappe.ui.form.on('Stock Entry', {
             });
         }
     },
-
+    validate_user_defaults(frm){
+        if(!USER_BRANCH && !IS_SUPER_USER){
+            frappe.alert(
+                "Your user does not have a default branch set. Please contact your administrator.", 
+                {indicator: 'red'}
+            );
+        }
+        if(!USER_WAREHOUSE && USER_BRANCH){
+            frappe.alert(
+                "Your branch does not have a default warehouse set. Please contact your administrator.", 
+                {indicator: 'red'}
+            );
+        }
+    }
 })
